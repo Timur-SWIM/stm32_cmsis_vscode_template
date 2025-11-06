78f0707a2ec0c5a49204c04d2f32ddfeff27e694
Timur <tmyaldzin13@gmail.com>
2025-11-06 23:50:14 +0300

    Обновлены определения светодиодов в заголовочном файле global.h

--- Inc/global.h
+++ Inc/global.h
@@ -6,10 +6,10 @@
 #define LED_2 0x005B
 #define LED_3 0x004F
 #define LED_4 0x0066
-#define LED_5 0x006D
-#define LED_6 0x007D
-#define LED_7 0x0007
-#define LED_8 0x007F
-#define LED_9 0x006F
+#define LED_5 0x000A
+#define LED_6 0x000B
+#define LED_7 0x000C
+#define LED_8 0x000D
+#define LED_9 0x000E
 
 #endif // GLOBAL_H
