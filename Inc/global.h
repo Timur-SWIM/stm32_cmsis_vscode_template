feda1eac8222c710c8c5f28c22d3e9956b32d82f
Timur <tmyaldzin13@gmail.com>
2025-11-06 23:44:10 +0300

    Добавлены новые зависимости и обновлены определения светодиодов в заголовочных файлах

---
+++ Inc/global.h
@@ -0,0 +1,15 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#define LED_0 0x0003F
+#define LED_1 0x0006
+#define LED_2 0x005B
+#define LED_3 0x004F
+#define LED_4 0x0066
+#define LED_5 0x006D
+#define LED_6 0x007D
+#define LED_7 0x0007
+#define LED_8 0x007F
+#define LED_9 0x006F
+
+#endif // GLOBAL_H
--- Inc/main.h
+++ Inc/main.h
@@ -1,10 +1,13 @@
+
 #ifndef MAIN_H_
 #define MAIN_H_
+#include "stm32f1xx.h"
 
-#include "stm32f103xb.h"
+#define SET_LED_NUM(digit) GPIOC->ODR = GPIOC->ODR == 0 ? 0 : digit
+#define LED_NUM_SWAP(digit)	GPIOC->ODR = GPIOC->ODR == 0 ? digit : 0;
+
+#define LIMIT_UP_CNT()  (TIM3->CNT = (TIM3->CNT > 72 ? 72 : TIM3->CNT))
+#define LIMIT_DOWN_CNT() (TIM3->CNT = (TIM3->CNT < 36 ? 36 : TIM3->CNT))
+void delay(uint32_t delay_value);
 
 #endif /* MAIN_H_ */
-
-void delay(uint32_t takts);
-
-#define LED_SWAP()		(GPIOA->ODR ^= GPIO_ODR_ODR5);
